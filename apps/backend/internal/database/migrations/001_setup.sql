0cfd8512d1e0760d5a5f84785d269391fdc92f8d
Samee Rauf <samee.rauf@pmg.com>
2025-10-12 15:30:25 -0700

    added function to convert all fields in a row into camel case & have functionality to convert from snake case to camel case when sending to frontend

--- apps/backend/internal/database/migrations/001_setup.sql
+++ apps/backend/internal/database/migrations/001_setup.sql
@@ -1,6 +1,32 @@
--- Write your migrate up statements here
+-- will take all fieds from a row and convert to camelCase
+CREATE OR REPLACE FUNCTION camel(input_row anyelement)
+    RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    result jsonb := '{}';
+    rec record;
+BEGIN
+    FOR rec IN
+    SELECT
+        lower(substring(regexp_replace(initcap(regexp_replace(key, '_', ' ', 'g')), '\s', '', 'g'), 1, 1)) || substring(regexp_replace(initcap(regexp_replace(key, '_', ' ', 'g')), '\s', '', 'g'), 2) AS camel_key,
+        value
+    FROM
+        jsonb_each(to_jsonb(input_row))
+        LOOP
+            result := result || jsonb_build_object(rec.camel_key, rec.value);
+        END LOOP;
+    RETURN result;
+END;
+$$;
 
----- create above / drop below ----
+-- Create updated_at trigger function
+CREATE OR REPLACE FUNCTION trigger_set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
--- Write your migrate down statements here. If this migration is irreversible
--- Then delete the separator line above.
+-- database schema will be like first_name; but when sending to frontend in json it will be firstName camelCase
