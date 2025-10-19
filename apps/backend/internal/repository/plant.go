91af2cb2466d9fae19bf3b4fe0466ba774dd94e1
Samee Rauf <samee.rauf@pmg.com>
2025-10-19 12:04:45 -0500

    feature: repo method for getplantbyid

--- apps/backend/internal/repository/plant.go
+++ apps/backend/internal/repository/plant.go
@@ -47,10 +47,58 @@ stmt := `
 	if err != nil {
 		return nil, fmt.Errorf("failed to execute create plant query for user_id=%s title=%s: %w", userID, payload.Title, err)
 	}
-// use pgx library to deserialize row into a struct from the data base (collect one row)
+    // use pgx library to deserialize row into a struct from the data base (collect one row)
 	plantItem, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[plant.Plant])
 	if err != nil {
 		return nil, fmt.Errorf("failed to collect row from table:plants for user_id=%s title=%s: %w", userID, payload.Title, err)
+	}
+
+	return &plantItem, nil
+}
+
+func (r *PlantRepository) GetPlantByID(ctx context.Context, userID string, plantID uuid.UUID) (*plant.Plant, error) {
+	stmt := `
+	SELECT
+		p.*,
+
+		-- Aggregate all observations for this plant
+		COALESCE(
+			jsonb_agg(
+				to_jsonb(camel(obs))
+				ORDER BY obs.date DESC
+			) FILTER (
+				WHERE obs.id IS NOT NULL
+			),
+			'[]'::JSONB
+		) AS observations
+
+	FROM
+		plants p
+		LEFT JOIN observations obs
+			ON obs.plant_id = p.id
+			AND obs.user_id = @user_id
+
+	WHERE
+		p.id = @plant_id
+		AND p.user_id = @user_id
+
+	GROUP BY
+		p.id
+	HAVING
+		p.id IS NOT NULL
+	`
+
+	rows, err := r.server.DB.Pool.Query(ctx, stmt, pgx.NamedArgs{
+		"user_id":  userID,
+		"plant_id": plantID,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute get plant by id query for user_id=%s plant_id=%s: %w", userID, plantID, err)
+	}
+
+	plantItem, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[plant.Plant])
+	if err != nil {
+		return nil, fmt.Errorf("failed to collect row from table:plants for user_id=%s plant_id=%s: %w", userID, plantID, err)
 	}
 
 	return &plantItem, nil
