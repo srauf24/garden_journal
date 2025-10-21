ca121e137eeede599f9d3cd2aff9a6fcdfcb7330
Samee Rauf <samee.rauf@pmg.com>
2025-10-21 08:39:41 -0500

    feat: added create observation repo method

--- apps/backend/internal/repository/repositories.go
+++ apps/backend/internal/repository/repositories.go
@@ -3,11 +3,13 @@ package repository
 import "github.com/srauf24/gardenjournal/internal/server"
 
 type Repositories struct{
-    Plant * PlantRepository
+    Plant *PlantRepository
+    Observation *ObservationRepository
 }
 
 func NewRepositories(s *server.Server) *Repositories {
 	return &Repositories{
         Plant: NewPlantRepository(s),
+        Observation: NewObservationRepository(s),
 	}
 }
